f91f77f878f69bc8e8108c94c69603616ef90af9
Gordie825 <Gordie825@gmail.com>
2019-07-09 13:50:20 +0200

    added comments to the CSS file and changed the output to appear in the HTML instead of an alert or a pop up

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
-function myFunction(){
+
+function displayMessage(){
+   //This is where I get the value entered by the user
     var temperature = document.getElementById("temp").value;
     console.log(temperature + " is the number ");
-
-    if (temperature >= 26){
-        alert("Wear Shorts And Flip Flops");
+//These are the different conditions for each temperature
+    if (temperature >= 27){
+        document.getElementById("wear").innerHTML = "Wear Shorts And Flip Flops";
     } 
-    else if(temperature <=25 && temperature >= 20){
-        alert("Wear Long Pants And A T-Shirt");
+    else if(temperature <=26 && temperature >= 20){
+        document.getElementById("wear").innerHTML =  "Wear Long Pants And A T-Shirt";
     }
     else if(temperature <= 19 && temperature >= 15){
-        alert("Wear Pants And A Jacket");
+        document.getElementById("wear").innerHTML =  "Wear Pants And A Jacket";
     }
      else if(temperature <= 14 && temperature >= 10){
-        alert("Wear A Jacket, Boots And A Pair Of Jeans");
-    } else if(temperature <= 9 && temperature >= 3){
-        alert("Wear A Coat And Boots");
+        document.getElementById("wear").innerHTML =  "Wear A Jacket, Boots And A Pair Of Jeans";
+    } 
+    else if(temperature <= 9 && temperature >= 1){
+        document.getElementById("wear").innerHTML =  "Wear A Coat And Boots"
     } 
     else {
-        alert("Just Wear Half Of Your Wadrobe");
+        document.getElementById("wear").innerHTML =  "Just Wear Half Of Your Wadrobe";
     }
 }
 
